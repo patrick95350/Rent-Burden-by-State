6c5fb8b37d663c170dfeb075e2df8f1fa3ecd544
Patrick Rogers <patrick.rogers@library.ca.gov>
2024-10-11 09:38:00 -0700

    ToDos

--- Main_Rent_Burden_by_State.R
+++ Main_Rent_Burden_by_State.R
@@ -12,8 +12,9 @@
 #       data.csv
 
 # To Do
-#   1. Item 1
-#   2. Item 2
+#   1. Pull ACS Data
+#   2. Clean and format
+#   3. Export table
 
 # Clear the workspace
 remove(list=ls(all=TRUE))
@@ -24,7 +25,6 @@ library(httr)
 
 # Inits
 setwd(here::here())
-
 
 # Load Sourcefiles
 
