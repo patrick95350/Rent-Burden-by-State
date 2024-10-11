1f5cdba6a94461a2b87c5feb1422aa4bb94364c7
Patrick Rogers <patrick.rogers@library.ca.gov>
2024-10-11 09:26:33 -0700

    Initial commit

---
+++ Main_Rent_Burden_by_State.R
@@ -0,0 +1,40 @@
+### Rent Burden by State
+#     Pulls ACS data to produce summary table of rent burden by US staate by year
+#
+#     By Patrick Rogers, California Research Bureau
+#       October 2024
+#
+#     Uses the following Packages
+#       {here}
+#       {httr}
+#
+#     Uses the following data
+#       data.csv
+
+# To Do
+#   1. Item 1
+#   2. Item 2
+
+# Clear the workspace
+remove(list=ls(all=TRUE))
+
+# Load Packages
+library(here)
+library(httr)
+
+# Inits
+setwd(here::here())
+
+
+# Load Sourcefiles
+
+# User Parameters
+
+# Custom Functions ####
+
+
+# Step 1 ####
+
+# Step 2 ####
+
+# Step 3 ####
