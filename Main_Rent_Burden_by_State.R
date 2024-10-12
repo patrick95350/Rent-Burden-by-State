3e993a4926dea946c164d877f8e7b8df1fac0673
Patrick Rogers <patrick.rogers@library.ca.gov>
2024-10-11 17:51:22 -0700

    First coding pass

--- Main_Rent_Burden_by_State.R
+++ Main_Rent_Burden_by_State.R
@@ -6,7 +6,7 @@
 #
 #     Uses the following Packages
 #       {here}
-#       {httr}
+#       {httr2}
 #
 #     Uses the following data
 #       data.csv
@@ -16,25 +16,81 @@
 #   2. Clean and format
 #   3. Export table
 
+# Tables of  Interest:
+#   B25070_001E: Total households
+#   B25070_007E to B25070_010E: 30% of HH income or above
+
 # Clear the workspace
 remove(list=ls(all=TRUE))
 
 # Load Packages
 library(here)
-library(httr)
+library(httr2)
 
 # Inits
 setwd(here::here())
 
 # Load Sourcefiles
+source(here::here('private', 'private_acs_api_key.R'))
 
 # User Parameters
+numerator_tables <- c('B25070_007E', 'B25070_008E', 'B25070_009E', 'B25070_010E')
+denominator_table <- 'B25070_001E'
+tables <-  paste0(c('NAME', denominator_table, numerator_tables), collapse=',')
+
+years <- 2005:2022
 
 # Custom Functions ####
 
+# Loop over years ####
+for(year in years){
+  # Skip 2020
+  if(year == 2020){
+    acs_data_burden <- cbind(acs_data_burden, `2020`=NA)
+    acs_data_severe <- cbind(acs_data_severe, `2020`=NA)
+    next
+  }
+  # Pull ACS Data
+  
+  # Build request
+  request <- request(paste0('https://api.census.gov/data/', year, '/acs/acs1'))
+  request <- req_url_query(request,
+                           get = tables,
+                           'for' = 'state:*',
+                           key = my.key)
+  req_dry_run(request)
+  
+  # Response
+  response <- req_perform(request)
+  
+  # Parse JSON
+  if(resp_status(response) == 200){
+    data <- resp_body_json(response, simplifyVector = TRUE)
+    names <- data[1,]
+    data <- data.frame(data[-1,])
+    colnames(data) <- names
+  }
+  
+  # Convert to numeric
+  data[,c(denominator_table, numerator_tables)] <- sapply(data[,c(denominator_table, numerator_tables)], as.numeric)
+  
+  # Initialize acs_data table
+  if(year == years[1]){
+    acs_data_burden <- data.frame(State = data$NAME)
+    acs_data_severe <- data.frame(State = data$NAME)
+  }
+  
+  # Calculate % Rent Burdened
+  acs_data_burden <- merge(acs_data_burden,
+                           data.frame(State = data$NAME, var = rowSums(data[,numerator_tables])/data[,denominator_table]), by  = 'State')
+  
+  # Calculate % Rent Burdened
+  acs_data_severe <- merge(acs_data_severe,
+                           data.frame(State = data$NAME, var = data[,'B25070_010E']/data[,denominator_table]), by  = 'State')
+}
 
-# Step 1 ####
-
+colnames(acs_data_burden) <- c('State', years)
+colnames(acs_data_severe) <- c('State', years)
 # Step 2 ####
 
 # Step 3 ####
